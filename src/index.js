247769982541e53b8952f86cbd3f073f2568ff08
alexharper84 <alexharper84@gmail.com>
2019-02-10 22:23:49 -0800

    state refactoring

--- src/index.js
+++ src/index.js
@@ -1,22 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// React requires that a dev define render
 class App extends React.Component {
   constructor(props) {
     super(props);
 
+    // THE ONLY SITUATION to make a direct assignment to this.state
+    // is inside the constructor function
     this.state = { lat: null };
-  }
 
-  render() {
     window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
+      position => {
+        // to update our state object, we call setState
+        // never make direct assignment to the state object
+        this.setState({ lat: position.coords.latitude });
+      },
+
       err => console.log(err)
     );
-
-    return <div>Latitude: </div>;
+  }
+  // React requires that a dev define render
+  render() {
+    return <div>Latitude: {this.state.lat} </div>;
   }
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
+
+// Some things to remember about State
+
+// *'State' is a JS object that contains data relevant to a component
+// *Updating 'state' on a component causes the component to (almost)
+// instantly rerender
+// *State must be initialized when a component is created
+// *state can only be updated using the function 'setState'
