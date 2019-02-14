efd9d7f0c0b6eb29a825035081c2f622acf564b5
alexharper84 <alexharper84@gmail.com>
2019-02-13 22:08:09 -0800

    Updates

---
+++ precache-manifest.740692b681ee62cc99187ee6e5eb7d15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd2c9a03e9ab3b0f65c8",
+    "url": "/seasons/static/css/main.6f89980d.chunk.css"
+  },
+  {
+    "revision": "fd2c9a03e9ab3b0f65c8",
+    "url": "/seasons/static/js/main.fd2c9a03.chunk.js"
+  },
+  {
+    "revision": "5b7ddef5528d7571d9bf",
+    "url": "/seasons/static/js/1.5b7ddef5.chunk.js"
+  },
+  {
+    "revision": "b162515c83c554475e0d",
+    "url": "/seasons/static/js/runtime~main.b162515c.js"
+  },
+  {
+    "revision": "53ca255f8f19faca604a8ccca310d0ac",
+    "url": "/seasons/index.html"
+  }
+];
